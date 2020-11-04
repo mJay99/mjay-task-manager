52a99489ec11e3b6c561eb244f3cfe4aaab37201
JayKumo <jayeshmali299@gmail.com>
2020-11-05 01:04:57 +0530

    TaskManager app layout setup.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ HeaderComponent,
+    DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class LayoutModule { }
--- src/app/task-manager/task-manager.module.ts
+++ src/app/task-manager/task-manager.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateUpdateTaskComponent } from './create-update-task/create-update-task.component';
 import { ListUsersComponent } from './list-users/list-users.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [CreateUpdateTaskComponent, ListUsersComponent],
+  declarations: [CreateUpdateTaskComponent, ListUsersComponent, ListTasksComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    // NgbModule,
+    RouterModule,
   ]
 })
 export class TaskManagerModule { }
