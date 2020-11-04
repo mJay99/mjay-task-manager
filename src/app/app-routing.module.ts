e070b872655f60b4a43a26fd3c719b5a53601e8d
JayKumo <jayeshmali299@gmail.com>
2020-11-05 03:00:40 +0530

    Board component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { BoardsComponent } from './task-manager/boards/boards.component';
 import { ListTasksComponent } from './task-manager/list-tasks/list-tasks.component';
 
 
 const routes: Routes = [
-  { path: '', component: ListTasksComponent },
+  // { path: '', component: ListTasksComponent },
   // { path: 'login', component: DefaultLoginComponent },
 
   {
     path: 'dashboard', component: DashboardComponent,
     canActivate: [],
     children: [
+      { path: '', component: BoardsComponent },
+      { path: 'boards', component: BoardsComponent },
       { path: 'tasks', component: ListTasksComponent },
      
     ]
