e91b57f4596e12178c91266c3ba7dcdb5cd85e95
JayKumo <jayeshmali299@gmail.com>
2020-11-06 12:30:07 +0530

    All layout components

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(   private router: Router,) { }
 
   ngOnInit() {
   }
 
+
+  public navigateTo(route_name: string) {
+    var self = this;
+    self.router.navigate([route_name]);
+  }
+  
 }
--- src/app/task-manager/boards/boards.component.ts
+++ src/app/task-manager/boards/boards.component.ts
@@ -11,7 +11,7 @@ export class BoardsComponent implements OnInit {
 
   ngOnInit() {
   }
-  navigateTo(){
+  navigateTo(param?){
     
   }
 
